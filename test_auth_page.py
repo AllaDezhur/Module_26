43b45d7b592f97eec44533191abcdbd65ca973b8
AllaDezhur <123001126+AllaDezhur@users.noreply.github.com>
2023-04-06 19:56:23 +0300

    Add files via upload

---
+++ test_auth_page.py
@@ -0,0 +1,14 @@
+from pages.auth_page import AuthPage
+import time
+
+
+def test_auth_page(selenium):
+   page = AuthPage(selenium)
+   page.enter_email("abcdf@mail.ru")
+   page.enter_pass("12345")
+   page.btn_click()
+
+   #знак != или == будет зависеть от того, верные или неверные данные мы вводим
+   assert page.get_relative_link() != '/all_pets', "login error"
+
+   time.sleep(5) #задержка для учебных целей
